e32f838363c0c318d733d7129f17ac2f3f8feb54
Adrian Bilescu <adrian.bilescu@gmail.com>
2021-02-09 16:18:30 +0200

    Reimplemented refresh control for scroll views using the introspect scroll view method.

--- Examples/SceneDelegate.swift
+++ Examples/SceneDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import SwiftUI
+import SwiftUIRefresh
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -7,7 +8,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
-        let contentView = ContentView()
+        let contentView = NavigationView {
+            RefreshableScrollView {
+                
+                Text("Text 1")
+                Text("Text 2")
+                Text("Text 3")
+            } onRefresh: { completion in
+                debugPrint("Start Refreshing")
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    debugPrint("End Refreshing")
+                    completion()
+                }
+            }
+            .navigationBarTitle("Your items")
+        }
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(rootView: contentView)
---
+++ Sources/RefreshableScrollView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+public struct RefreshableScrollView<Content: View>: View {
+    class ViewModel: ObservableObject {
+        weak var refreshControl: UIRefreshControl?
+        var onRefresh: (@escaping () -> Void) -> Void
+        
+        init(onRefresh: @escaping (@escaping () -> Void) -> Void) {
+            self.onRefresh = onRefresh
+        }
+        
+        @objc func refreshContent(_ sender: UIRefreshControl) {
+            onRefresh { [weak self] in
+                self?.refreshControl?.endRefreshing()
+            }
+        }
+        
+        func endRefreshing() {
+            refreshControl?.endRefreshing()
+        }
+    }
+    
+    @State private var isShowing = false
+    @ObservedObject var viewModel: ViewModel
+    var content: Content
+    
+    public init(@ViewBuilder content: () -> Content, onRefresh: @escaping (@escaping () -> Void) -> Void) {
+        self.content = content()
+        self.viewModel = .init(onRefresh: onRefresh)
+    }
+    
+    public var body: some View {
+        ScrollView {
+            content
+        }
+        .introspectScrollView { scrollView in
+            if self.viewModel.refreshControl != nil { return }
+            let refreshControl = UIRefreshControl()
+            refreshControl.addTarget(self.viewModel, action: #selector(self.viewModel.refreshContent(_:)), for: .valueChanged)
+            scrollView.refreshControl = refreshControl
+            self.viewModel.refreshControl = refreshControl
+        }
+    }
+}
